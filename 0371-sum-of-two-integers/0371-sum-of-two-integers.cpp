f905cf4772719c8b992295188597c76d194674cf
harsh <Harsh24235@iiitd.ac.in>
2026-06-29 13:32:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (34.94%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -2,28 +2,12 @@ class Solution {
 public:
     int getSum(int a, int b) {
 
-        int num1 = a;
-        int num2 = b;
-
-        int ans = 0;
-        int carry = 0;
-
-        for(int i = 0  ; i < 32 ; i++){
-
-            int lastbitOfnum1 = num1 & 1;
-            int lastbitOfnum2 = num2 & 1;
-            num1 = num1 >> 1;
-            num2 = num2 >> 1;
-
-            int bitsum = lastbitOfnum1 ^ lastbitOfnum2 ^ carry;
-            
-            carry = (lastbitOfnum1 & lastbitOfnum2) |
-                    (lastbitOfnum2 & carry) |
-                    (lastbitOfnum1 & carry);
-
-            ans = ans | (bitsum << i);
+        while(b != 0){
+            int carry =  (a & b) << 1;
+            a = a ^ b;
+            b = carry;
         }
-
-        return ans;
+        
+        return a;
     }
 };
