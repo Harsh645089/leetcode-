82bd02fab1f74c2c34706389fe6e5e7315ff64ca
harsh <Harsh24235@iiitd.ac.in>
2026-07-17 23:31:57 +0530

    Time: 3 ms (37.73%), Space: 41.1 MB (43.25%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -8,23 +8,20 @@ public:
         //oth element ke left 
         leftproduct[0] = 1;
 
-        //n-1th ke right main
-        rightproduct[n-1] = 1;
-
         for(int i = 1 ; i < n ; i++){
             leftproduct[i] = leftproduct[i-1] * nums[i-1];
         }
 
-        for(int i = n-2 ; i >= 0 ; i--){
-            rightproduct[i] = rightproduct[i+1] * nums[i+1];
+        int rp = 1;
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            leftproduct[i] = leftproduct[i] * rp;
+            rp = rp * nums[i] ;
         }
 
-        vector<int> result(n);
+        return leftproduct;
 
-        for(int i = 0 ; i < n ;i++){
-            result[i] = leftproduct[i] * rightproduct[i];
-        }
 
-        return result;
+
     }
 };
