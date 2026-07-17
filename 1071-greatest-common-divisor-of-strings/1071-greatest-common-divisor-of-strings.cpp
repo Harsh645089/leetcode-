810adbdf61a83e44e377f49c6ce98c3bef165283
harsh <Harsh24235@iiitd.ac.in>
2026-07-17 15:28:12 +0530

    Time: 0 ms (100%), Space: 9.8 MB (60.87%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,43 +1,13 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        int str1Len = str1.size();
-        int str2Len = str2.size();
-
-        string ans = "";
-
-        for(int i = str1Len ; i > 0 ; i--){
-            //working on  string 1
-            string x = str1.substr(0 , i);
-            int xLen = x.size();
-
-            if(str1Len % xLen == 0 && str2Len % xLen == 0){
-                //means we can consider it
-                int factor = str1Len / xLen ;
-
-                string temp = "";
-
-                for(int j = 0 ; j < factor ; j++){
-                    temp+=x;
-                }
-
-                if(temp == str1){
-                    factor = str2Len / xLen ;
-                    string temp2 = "";
-
-                    for(int j = 0 ; j < factor ; j++){
-                        temp2+=x;
-                    }
-
-                    if(temp2 == str2){
-                        if(ans.size() < xLen){
-                            ans = x;
-                        }
-                    }
-                }
-            }
+        if(str1 + str2 != str2 + str1){
+            //means they have no common thing 
+            return "";
         }
 
-        return ans;
+        int len = gcd(str1.size()  , str2.size());
+
+        return str1.substr(0 , len);
     }
 };
