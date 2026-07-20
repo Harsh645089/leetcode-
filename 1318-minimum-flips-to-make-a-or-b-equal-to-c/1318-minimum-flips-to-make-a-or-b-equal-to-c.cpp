d839c758c89a83288a57217fd57c5d765ca42dd6
harsh <Harsh24235@iiitd.ac.in>
2026-07-20 17:25:38 +0530

    Time: 2 ms (53.7%), Space: 7.9 MB (39.46%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -3,19 +3,23 @@ public:
     int minFlips(int a, int b, int c) {
         int count = 0;
         for(int i = 0 ; i < 32 ; i++){
-            if(((1 << i) & c) != 0){
+            int abit = (1 << i) & a ;
+            int bbit = (1 << i) & b ;
+            int cbit = (1 << i) & c ;
+
+            if(cbit != 0){
                 //means is active 
-                if(((1 << i) & a) == 0 && ((1 << i) & b) == 0){
+                if(abit == 0 && bbit == 0){
                     count++;
                 }
             }
             else{
                 //means that bit is not active
-                if(((1 << i) & a) != 0){
+                if(abit!= 0){
                     //means a is active
                     count++;
                 }
-                if(((1 << i) & b) != 0){
+                if(bbit != 0){
                     //b is also active
                     count++;
                 }
