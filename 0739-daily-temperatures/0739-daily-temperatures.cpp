825eed28956f0e1b04b4df8eb3a8a85c0ca5d05b
harsh <Harsh24235@iiitd.ac.in>
2026-07-20 16:28:56 +0530

    Time: 27 ms (34.12%), Space: 102.9 MB (62.89%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -5,26 +5,19 @@ public:
         stack<int> st;
         vector<int> ans(n , 0);
         for(int i = n -1 ; i >= 0 ; i--){
-            if(st.empty()){
-                //no element in the stack mean there is no one that warmer thann this 
-                ans[i] = 0;
-                st.push(i);
+            //there are elements in the stack 
+            while(!st.empty() && temperatures[i] >= temperatures[st.top()]){
+                st.pop(); // remving the invariant elements
+            }
+
+            if(!st.empty()){
+                ans[i] = st.top() - i; // as now the top element is the first warmer element  
             }
             else{
-                //there are elements in the stack 
-                while(!st.empty() && temperatures[i] >= temperatures[st.top()]){
-                    st.pop(); // remving the invariant elements
-                }
-
-                if(!st.empty()){
-                    ans[i] = st.top() - i; // as now the top element is the first warmer element 
-                    st.push(i); //as this is smaller than the top so this can be a ans for some temp
-                }
-                else{
-                    ans[i] = 0;
-                    st.push(i);
-                }
+                ans[i] = 0;
             }
+            st.push(i);  //as this is smaller than the top so this can be a ans for some temp
+        
         }
 
         return ans;
