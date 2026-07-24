78a2021c6eb8983a04e5a382e414b5f4616c5335
harsh <Harsh24235@iiitd.ac.in>
2026-07-24 22:37:40 +0530

    Time: 155 ms (6.34%), Space: 158 MB (5.2%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    int solve(vector<int> &prices , int n , int bought , int i , vector<vector<int>> &dp){
-        if(i >= n) return 0; //now agar bought hai toh can't sell it or if not bought yet then cannot bought
-
-        if(dp[i][bought] != -1) return dp[i][bought];
-
-        if(bought == true){
-            return dp[i][bought] = max(prices[i]  , 
-                        solve(prices , n , true ,i+1 , dp));
-        }
-        else{
-            return dp[i][bought] = max(solve(prices , n , true , i+ 1 , dp) - prices[i] , 
-                        solve(prices , n , false , i+ 1 , dp));
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int bought = false;
-        vector<vector<int>> dp(n , vector<int>(2 , -1));
-        return solve(prices , n , bought , 0 , dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2 , 0));
+
+        for(int i = n - 1  ; i >= 0 ; i--){
+            dp[i][0] = max(dp[i+1][1] - prices[i] , dp[i+1][0]);
+            dp[i][1] = max(dp[i+1][1] , prices[i]);
+        }
+
+        return dp[0][0];
 
     }
 };
