059595417f950702c118b25eb076bca99bbb7ba4
harsh <Harsh24235@iiitd.ac.in>
2026-07-31 00:34:05 +0530

    Time: 12 ms (90.94%), Space: 37.3 MB (57.53%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> num1;
+
+        unordered_set<int> set1;
         for(auto num : nums1){
-            num1.insert(num);
+            set1.insert(num);
         }
 
-        unordered_set<int> common;
+        unordered_set<int> set2;
         for(auto num : nums2){
-            if(num1.find(num) != num1.end()){
-                //means it is common
-                common.insert(num);
-            }
+            set2.insert(num);
         }
-        unordered_set<int> zeroth;
+
         vector<int> ans1;
-        for(auto num : nums1){
-            if(common.find(num) == common.end()){
-                zeroth.insert(num);
+        vector<int> ans2;
+
+        for(auto num : set1){
+            if(set2.find(num) == set2.end()){
+                //means not common
+                ans1.push_back(num);
             }
         }
 
-        for(auto num : zeroth){
-            ans1.push_back(num);
+        for(auto num : set2){
+            if(set1.find(num) == set2.end()){
+                //not common
+                ans2.push_back(num);
+            }
         }
         
-
-        unordered_set<int> first;
-        vector<int> ans2;
-        for(auto num : nums2){
-            if(common.find(num) == common.end()){
-                first.insert(num);
-            }
-        }
-
-        for(auto num : first){
-            ans2.push_back(num);
-        }
-
         return {ans1, ans2};
 
 
