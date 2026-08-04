1b71deed2ab380d49ed5e84df4aea4fb53934b3e
harsh <Harsh24235@iiitd.ac.in>
2026-08-04 12:40:36 +0530

    Time: 3 ms (89.28%), Space: 23.5 MB (48.5%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size() != word2.size()) return false;
-
-        unordered_map<char , int> mp1;
+        vector<int> v1(26 , 0);
         for(auto ch : word1){
-            mp1[ch]++;
+            v1[ch - 'a']++;
         }
-
-        unordered_map<char , int> mp2;
+       
+        vector<int> v2(26 , 0);
         for(auto ch : word2){
-            mp2[ch]++;
+            v2[ch - 'a']++;
         }
 
-        for(auto ch : word2){
-            if(mp1.find(ch) == mp1.end()) return false;
+        for(int i = 0; i < 26 ; i++){
+            //there should be only character that present in both
+            if((v1[i] == 0) != (v2[i] == 0)){
+                return false;
+            }
         }
-
-        for(auto ch : word1){
-            if(mp2.find(ch) == mp2.end()) return false;
-        }
-
-        vector<int> v1;
-        for(auto p : mp1){
-            v1.push_back(p.second);
-        }
-
-        vector<int> v2;
-        for(auto p : mp2){
-            v2.push_back(p.second);
-        }
+        
 
         sort(v1.begin() , v1.end());
         sort(v2.begin() , v2.end());
