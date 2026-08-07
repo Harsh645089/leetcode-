028e66514973425ab6e8f74ea5abcee017c55fa0
harsh <Harsh24235@iiitd.ac.in>
2026-08-07 16:33:14 +0530

    Time: 2167 ms (5.01%), Space: 69 MB (91.03%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,13 +1,46 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int , vector<int> , greater<int>> pq;
+    int partition(vector<int> &nums  , int low , int high ){
 
-        for(auto num : nums){
-            pq.push(num);
-            if(pq.size() > k) pq.pop();
+        int randomidx = low + rand() % (high - low + 1); // Generates a random index in the range [low, high]
+        swap(nums[randomidx] , nums[high]); // this is needed to improve the speed of algo
+
+
+        int pivot = nums[high];
+        int i = low;
+
+        for(int j = low ; j < high ; j++){
+            if(nums[j] < pivot){
+                swap(nums[j] , nums[i]);
+                i++;
+            }
         }
+
+        swap(nums[i] , nums[high]); //cannot use pivot here as it is only the copy of the nums[high] not the nums[high] so if i used that then nothing will chnage in the array 
+
+        return i;
+    }
+    
+    int quickselect(vector<int> &nums  , int low , int high ,int &idx){
+
+        if(low > high) return -1;
         
-        return pq.top();
+        int pivotidx = partition(nums , low , high);
+
+        if(pivotidx == idx) return nums[pivotidx];
+
+        else if(pivotidx < idx) return quickselect(nums , pivotidx + 1 , high , idx);
+
+        return quickselect(nums , low , pivotidx -1 , idx);
+    }
+
+
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        int idx = n - k ; //if it is a sorted array then the kth element from the right is the kth largest element so the its idx from left is n - k so i need to find the correct element of this idx
+
+        return quickselect(nums , 0 , n-1 , idx);
+
+        
     }
 };
