4c00dd5da4af2f0628ebe86229d857d5eba759d8
harsh <Harsh24235@iiitd.ac.in>
2026-08-11 21:10:49 +0530

    Time: 2 ms (71.55%), Space: 10.8 MB (93.83%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -14,13 +14,15 @@ public:
             int windowlen = right - low + 1;
 
             //if the window is not valid then this will make it valid
-            if(windowlen - maxfreq  > k){
+            while(windowlen - maxfreq  > k){
                 //means this window is not valid one
                 freq[s[low] - 'A']--;
                 low++;
+                windowlen = right - low + 1;
+                
             }
             
-            ans = max(ans ,right - low + 1); //i have used the formula instead of windowlen as that may be not the valid one but if i used the formula then it only update the ans if it's size is more than the last valid one otherwise the invalid one will not able to update the ans as we are increasing the low for the invlaid window that decrease the window size then the last valid one , we can use while also but that increase the tc and this is also unnecesary as this can deal with the case with less tc 
+            ans = windowlen; //i have used the formula instead of windowlen as that may be not the valid one but if i used the formula then it only update the ans if it's size is more than the last valid one otherwise the invalid one will not able to update the ans as we are increasing the low for the invlaid window that decrease the window size then the last valid one , we can use while also but that increase the tc and this is also unnecesary as this can deal with the case with less tc 
         }
         return ans;
     }
